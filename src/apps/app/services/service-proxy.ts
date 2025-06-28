4e39600677928fb39dab5302ec0cc4457c65c064
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-06-28 21:55:44 +0200

    Create service proxy fetch info

---
+++ src/apps/app/services/service-proxy.ts
@@ -0,0 +1,22 @@
+import { buildServerReq } from '@libs/utils';
+import { makeRequest } from './make-request';
+import { internalServerError } from '@libs/errors';
+
+export const fetchInfo = async (
+  serviceInfo: SF.IServiceInfo,
+  dataBody: any,
+) => {
+  const { proxyReq, method, url } = buildServerReq(serviceInfo, dataBody);
+  const { data, message, success } = await makeRequest(
+    proxyReq,
+    url,
+    method,
+    dataBody,
+  );
+
+  if (!success) {
+    throw internalServerError(message || 'Failed to fetch info');
+  }
+
+  return data;
+};
