803c071a64f148210b6c7bb04b753fad618ea798
Jean dAmour AKIMANA(MBP) <akimanaja17@gmail.com>
2025-07-07 07:17:12 +0200

    Update the testing port

--- src/configs/security.ts
+++ src/configs/security.ts
@@ -19,7 +19,7 @@ export function applySecurity(app: Express) {
  */
 export const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:52521',
+  'http://localhost:64385',
 ];
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
