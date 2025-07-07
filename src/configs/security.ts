67855d4e52c64ef5f907dd01a4c7864f30819071
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-07-07 19:46:13 +0200

    Update the testing env

--- src/configs/security.ts
+++ src/configs/security.ts
@@ -19,7 +19,7 @@ export function applySecurity(app: Express) {
  */
 export const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:59101',
+  'http://localhost:62784',
 ];
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
