ccba464b7ac9031cf0df37fd7920dec93407f59e
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-07-08 13:48:53 +0200

    Add new env

--- src/configs/security.ts
+++ src/configs/security.ts
@@ -19,7 +19,7 @@ export function applySecurity(app: Express) {
  */
 export const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:49738',
+  'http://localhost:52624',
 ];
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
