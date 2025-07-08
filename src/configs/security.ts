1ebebfa4f15f884d63c9dba073e6256830601b77
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-07-08 11:50:37 +0200

    Update debug port

--- src/configs/security.ts
+++ src/configs/security.ts
@@ -19,7 +19,7 @@ export function applySecurity(app: Express) {
  */
 export const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:62784',
+  'http://localhost:49738',
 ];
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
