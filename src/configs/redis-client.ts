9825136e69df2b0dad382ecb79157f55a0c7b3e7
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-07-21 19:05:50 +0200

    Initialize redis

--- src/configs/redis-client.ts
+++ src/configs/redis-client.ts
@@ -1,12 +1,12 @@
 import { RedisStore } from 'connect-redis';
 import { createClient } from 'redis';
 
-let redisClient;
+let redisClient: ReturnType<typeof createClient> | null = null;
 
 export const initializeRedisClient = async () => {
   // read the Redis connection URL from the envs
   let redisURL = process.env.REDIS_URI;
-  if (redisURL) {
+  if (redisURL && !redisClient?.isOpen) {
     // create the Redis client object
     redisClient = createClient({ url: redisURL }).on('error', (e) => {
       console.error(`Failed to create the Redis client with error:`);
@@ -24,7 +24,37 @@ export const initializeRedisClient = async () => {
   }
 };
 
+export const getRedisClient = () => {
+  if (!redisClient) {
+    throw new Error(
+      'Redis client is not initialized. Call initializeRedisClient first.',
+    );
+  }
+  return redisClient;
+};
+
+export const closeRedisClient = async () => {
+  if (redisClient) {
+    try {
+      await redisClient.quit();
+      console.log('Redis client closed successfully.');
+    } catch (e) {
+      console.error('Error closing Redis client:', e);
+    }
+  } else {
+    console.warn('Redis client is not initialized, nothing to close.');
+  }
+};
+
+initializeRedisClient()
+  .then(() => {
+    console.log('Redis client initialized successfully.');
+  })
+  .catch((error) => {
+    console.error('Error initializing Redis client:', error);
+  });
+
 export const redisStore = new RedisStore({
   prefix: process.env.REDIS_PREFIX,
-  client: redisClient,
+  client: getRedisClient(),
 });
