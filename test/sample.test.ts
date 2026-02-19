af58d4722adf7e6c3f4f00fa09a3d84442614dca
Akimana A Jean d Amour <akimanaja17@gmail.com>
2026-02-19 17:03:37 +0200

    Fix tests

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,13 +1,12 @@
 import * as chai from 'chai';
-import chaiHttp from 'chai-http';
+import request from 'supertest';
 import app from '../src/app';
 
-chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('Sample Route Test', () => {
   it('should return 404 for an unknown route', async () => {
-    const res = await (chai as any).request(app).get('/unknown-route');
-    expect(res).to.have.status(404);
+    const res = await request(app).get('/unknown-route');
+    expect(res.status).to.equal(404);
   });
 });
