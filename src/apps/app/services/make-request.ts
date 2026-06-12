9d118ce4babef496739b24c8431696d9024a89cb
Akimana A Jean d Amour <akimanaja17@gmail.com>
2026-06-12 11:12:13 +0200

    Add a fix

--- src/apps/app/services/make-request.ts
+++ src/apps/app/services/make-request.ts
@@ -65,7 +65,6 @@ export async function makeRequest<T>(
       error: {
         message: error.message,
         response: error.response?.data,
-        request: error.request,
       },
     });
     return errorRes;
